cbaaa886e485d7ac7f0701794322fd8ca860efc3
harden13 <80114019@qq.com>
2018-05-23 14:19:10 +0800

    项目环境配置和用户表设计

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('avatar'); //头像
+            $table->string('confirmation_token'); //激活token
+            $table->smallInteger('is_active')->default(0); //是否激活
+            $table->integer('questions_count')->default(0); //问题数
+            $table->integer('answers_count')->default(0); //回答数
+            $table->integer('comments_count')->default(0); //评论数
+            $table->integer('favorites_count')->default(0); //收藏数
+            $table->integer('likes_count')->default(0);
+            $table->integer('followers_count')->default(0); //关注数
+            $table->integer('followings_count')->default(0); //被关注数
+            $table->json('settings')->nullable(); //用户信息
             $table->rememberToken();
             $table->timestamps();
         });
