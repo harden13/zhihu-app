1c36a08c2e39ea10d111fd51da9de5fd04f7e566
harden13 <80114019@qq.com>
2018-06-01 15:51:29 +0800

    用户关注问题2

--- app/Http/Controllers/QuestionFollowController.php
+++ app/Http/Controllers/QuestionFollowController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Auth;
-use Illuminate\Http\Request;
 
 class QuestionFollowController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function follow($question)
     {
-        Auth::user()->follows($question);
+        Auth::user()->followThis($question);
 
         return back();
     }
--- app/User.php
+++ app/User.php
@@ -56,11 +56,18 @@ class User extends Authenticatable
         return $this->hasMany(Answer::class);
     }
 
-    public function follows($question)
+    public function follows()
     {
-        Follow::create([
-            'question_id' => $question,
-            'user_id' => $this->id
-        ]);
+        return $this->belongsToMany(Question::class, 'user_question')->withTimestamps();
+    }
+
+    public function followThis($question)
+    {
+        return $this->follows()->toggle($question);
+    }
+
+    public function followed($question)
+    {
+        return !! $this->follows()->where('question_id', $question)->count();
     }
 }
