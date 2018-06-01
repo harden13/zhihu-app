9ab5c5591883c5d27441ed9eb61d50ecb46c9fc8
harden13 <80114019@qq.com>
2018-06-01 17:08:33 +0800

    使用 Vuejs 组件化

--- app/Http/Controllers/QuestionFollowController.php
+++ app/Http/Controllers/QuestionFollowController.php
@@ -10,7 +10,7 @@ class QuestionFollowController extends Controller
     {
         $this->middleware('auth');
     }
-    
+
     public function follow($question)
     {
         Auth::user()->followThis($question);
---
+++ resources/assets/js/components/QuestionFollowButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <button class="btn btn-default" v-text="text" v-on:click="follow" v-bind:class="{'btn-success':followed}"></button>
+</template>
+
+<script>
+    export default {
+        props:['question', 'user'],
+        mounted() {
+            this.$http.post('/api/question/follower', {'question':this.question, 'user':this.user}).then(response => {
+                this.followed = response.data.followed
+            })
+        },
+        data() {
+            return {
+                followed: false
+            }
+        },
+        computed: {
+            text() {
+                return this.followed ? '已关注' : '关注该问题'
+            }
+        },
+        methods: {
+            follow() {
+                this.$http.post('/api/question/follow', {'question':this.question, 'user':this.user}).then(response => {
+                    this.followed = response.data.followed
+                })
+            }
+        }
+    }
+</script>
